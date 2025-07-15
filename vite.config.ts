c13cf3d3abb1610375e679cf8e948ce63fae0122
Shehan R.K <ruwanbrother1@gmail.com>
2025-07-15 13:49:38 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CeylonClean-Pro/', // 👈 use your repo name
   plugins: [react()],
-})
+  base:"/CeylonClen-Pro",
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
